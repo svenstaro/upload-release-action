12f7fc1a8b37fe4d548a9e127ff1ea0284c0870d
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-07-02 09:46:02 +0200

    Add stub tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,25 @@
+import * as github from '@actions/github'
 import * as main from '../src/main'
 
 // Frankly, tests would be entirely useless unless we can mock GitHub somehow.
 describe('Upload Release Action', () => {
-  it('', async () => {})
+  let spyGetOctokit: jest.SpyInstance<any>
+  let mockOctokit: any
+
+  beforeEach(() => {
+    mockOctokit = {
+      repos: {
+        createRelease: jest.fn(async () => ({
+          data: {
+            id: 'lalala',
+            upload_url: 'oaoa'
+          }
+        }))
+      }
+    }
+    // spyGetOctokit = jest.spyOn(github, "getOctokit").mockImplementation(() => mockOctokit
+    // jest.spyOn(github, "context", "get").mockImplementation(() => "testtest");
+  })
+
+  it('pls write actual test', async () => {})
 })
